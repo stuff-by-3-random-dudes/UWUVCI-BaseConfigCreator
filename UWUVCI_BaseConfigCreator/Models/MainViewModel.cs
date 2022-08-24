907946ca68b3fdaee02553d1d11f935df1e9691c
ZestyTS <ZestyTS@gmail.com>
2022-08-24 09:20:03 -0700

    Setting up for .net 6

--- UWUVCI_BaseConfigCreator/Models/MainViewModel.cs
+++ UWUVCI_BaseConfigCreator/Models/MainViewModel.cs
@@ -121,7 +121,7 @@ namespace UWUVCI_BaseConfigCreator
             {
                 throw new GameBaseException($"A TitleKey with no value is not valid!");
             }
-            TempBase.KeyHash = NonHashedKey.ToLower().GetHashCode();
+            TempBase.KeyHash = GetDeterministicHashCode(NonHashedKey);
             NonHashedKey = string.Empty;
             LGameBases.Add(TempBase);
             TempBase = new GameBases();
@@ -220,6 +220,23 @@ namespace UWUVCI_BaseConfigCreator
                 LGameBases = backupList;
             }
 
+        }
+        public static int GetDeterministicHashCode(string str)
+        {
+            unchecked
+            {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+                return hash1 + (hash2 * 1566083941);
+            }
         }
     }
 }
